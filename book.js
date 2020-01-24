f1e69107b3bb1eeb952d615daeac44ebcd44a922
naz_liton <nazrulliton@icloud.com>
2020-01-24 13:19:39 +0000

    book class refactored1

--- book.js
+++ book.js
@@ -1,7 +1,10 @@
-const Book = function (author, title, genre) {
-  this.author = author;
-  this.title = title;
-  this.genre = genre;
-};
+class Book = {
+  constructor(author, title, genre) {
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+}
+
+
 
 module.exports = Book;
