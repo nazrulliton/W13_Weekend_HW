7e778b9bddf0e90ce6d5bf0b43ea1ad11e1b79a9
naz_liton <nazrulliton@icloud.com>
2020-01-24 13:23:36 +0000

    classes refactored

--- book.js
+++ book.js
@@ -3,6 +3,7 @@ class Book = {
     this.author = author;
     this.title = title;
     this.genre = genre;
+  }
 }
 
 
--- library.js
+++ library.js
@@ -1,20 +1,26 @@
-const Library = function (books = []) {
-  this.books = books;
-};
 
-Library.prototype.bookCount = function () {
+class Library = {
+  constructor (books = []) {
+    this.books = books;
+  }
+}
+
+
+
+
+Library.bookCount = function () {
   return this.books.length;
 };
 
-Library.prototype.addBook = function (newBook) {
+Library.addBook = function (newBook) {
   this.books.push(newBook);
 };
 
-Library.prototype.addBooks = function (newBooks) {
+Library.addBooks = function (newBooks) {
   newBooks.forEach(book => this.books.push(book));
 };
 
-Library.prototype.printInventory = function () {
+Library.printInventory = function () {
   this.books.forEach((book) => {
     console.log(`${book.title} by ${book.author}`);
   });
