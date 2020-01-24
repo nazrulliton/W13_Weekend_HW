4b1f636c1db401ef177dff9d2ebf04a0f49faf7d
naz_liton <nazrulliton@icloud.com>
2020-01-24 13:36:00 +0000

    all tests passing with new refactored classes

--- library.js
+++ library.js
@@ -3,27 +3,29 @@ class Library  {
   constructor (books = []) {
     this.books = books;
   }
+
+  bookCount = function () {
+    return this.books.length;
+  };
+
+  addBook = function (newBook) {
+    this.books.push(newBook);
+  };
+
+  addBooks = function (newBooks) {
+    newBooks.forEach(book => this.books.push(book));
+  };
+
+  printInventory = function () {
+    this.books.forEach((book) => {
+      console.log(`${book.title} by ${book.author}`);
+    });
+  };
 }
 
 
 
 
-Library.bookCount = function () {
-  return this.books.length;
-};
 
-Library.addBook = function (newBook) {
-  this.books.push(newBook);
-};
-
-Library.addBooks = function (newBooks) {
-  newBooks.forEach(book => this.books.push(book));
-};
-
-Library.printInventory = function () {
-  this.books.forEach((book) => {
-    console.log(`${book.title} by ${book.author}`);
-  });
-};
 
 module.exports = Library;
